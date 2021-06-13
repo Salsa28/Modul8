7e56ce41b10842cbc7b416beb1ebfde8885089b6
Lenovo ID <Lenovo ID@DESKTOP-ND0B9TH>
2021-06-13 18:34:42 +0700

    Tugas Modul 8

---
+++ src/BinaryTree/Treenode.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BinaryTree;
+
+/**
+ *
+ * @author Salsa Dwi Nur Hidayah
+ */
+public class Treenode {
+    char data;
+    Treenode left;
+    Treenode right;
+    
+    public Treenode(char data){
+        this.data=data;
+    }
+}
